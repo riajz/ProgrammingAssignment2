6f885b64d201fb13917c0b1b11a6c927ba8779b4
riajz <zekind@alumni.psu.edu>
2016-03-04 13:58:22 -0500

    git is a piece of rubbish and, considering the prerequisites of this course were "don't need programming or statistical expertise," the fact that this smoldering pile of fetid stank ends up being used (because I forked this stupid thing, and I made changes, but it's not sensing those changes for SOME REASON), seems WILD- LY inappropriate.
    This is my frustrated plea to figure out how to make this coursework a little
    easier to actually submit--maybe by submitting the stupid text file that contains
    the R code?

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## name: cachematrix.R
+## developer: john zekind (github.com/riajz)
+## date: 2016-03-04
+## desc: R Programming Week 3 (Assignment 2)
+## 
+## 
+## makeCacheMatrix - creates a cache of a provided matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+    
+    ## setting environment data outside of function for caching operations
+  
+    inv <- NULL
+    
+    set <- function(y){
+      x <<- y
+      inv <<- NULL
+    }
 
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    
+    ## returns list of functions for the matrix x:
+    ## 1. setting a matrix
+    ## 2. getting a matrix
+    ## 3. setting the inverse of the matrix
+    ## 4. getting the inverse of the matrix
+    ## --this is used for cacheSolve()
+    
+    list(set=set,
+         get=get,
+         setinv=setinv,
+         getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve - returns the inverse of a matrix, first by cache if exists,
+##   then by solving it directly
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## getting matrix from makeCacheMatrix
+  inv <- x$getinv()
+  
+  ## retrieve and return cached solution if available
+  if (!is.null(inv)){
+    message("Retrieving cached solution...")
+    return(inv)
+  }
+  else {
+    ## otherwise calc solution
+    message("Calculating solution...")
+    mat.data <- x$get()
+    inv <- solve(mat.data, ...)
+    
+    ## storing solution in cache for future retrieval
+    x$setinv(inv)
+    
+    ##return solution
+    return(inv)
+  }
+  
 }
